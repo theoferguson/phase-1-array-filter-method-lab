78727966478bdf915234d13d61ba0c13c78f6ec4
Theo Ferguson <ferguson.theo@gmail.com>
2021-08-11 20:52:34 -0400

    commit tree 79ee604f3731f76e52c88b8a32a7e4791b28fa59

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(driverNameArr, name) {
+    return driverNameArr.filter( e => e.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(fuzzyDriverName, string) {
+    return fuzzyDriverName.filter(function(el) {
+        return el.startsWith(string);
+    })
+}
+
+function matchName(driverObjects, string) {
+    return driverObjects.filter(function(el) {
+        return el.name === string;
+    })
+}
